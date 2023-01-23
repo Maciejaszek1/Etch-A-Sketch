be66a49f31cea714605ef9bd593a36f35a56d2af
Maciejaszek1 <maciek.sygula@gmail.com>
2023-01-23 15:32:15 +0100

    Paints every square hovered over by mouse

--- scripts.js
+++ scripts.js
@@ -4,8 +4,13 @@ for(let i = 0; i < 256; i++){
     const gridElement = document.createElement('div');
 
     gridElement.classList.add('gridElement');
+    gridElement.setAttribute("id", i);
 
     gridElement.textContent = i;   
     container.appendChild(gridElement);
 }
 
+const elements = document.querySelectorAll('.gridElement');
+elements.forEach(element => element.addEventListener('mouseover',function(){
+    element.style.backgroundColor = "yellow";
+}));
